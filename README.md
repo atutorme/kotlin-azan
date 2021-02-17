17010d8d0d1820d642e5cae83f382f356dd93a61
ATutorMe <atutorme@gmail.com>
2021-02-17 23:13:46 +1000

    clarified names of sun declination variables. see the wikipedia article

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kotlin-azan
 More idiomatic Kotlin implementation of Azan, Adhan, Azaan, Adhaan, Athan, Athaan, Salat, Salaat, Salah, Salaah, Namaz, Namaaz, Prayer times!
+
+## Formulae used
+http://praytimes.org/wiki/Prayer_Times_Calculation
+https://en.wikipedia.org/wiki/Position_of_the_Sun
--- src/main/kotlin/Sun.kt
+++ src/main/kotlin/Sun.kt
@@ -1,7 +1,23 @@
 import java.time.LocalDate
 import java.time.temporal.JulianFields
+import kotlin.math.asin
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
 
 object Sun {
     val localDate = LocalDate.now()
     val julianDate = JulianFields.JULIAN_DAY.getFrom(localDate)
+    val daysSinceJ2000 = julianDate - 2451545.0
+
+    val meanAnomaly = 357.529 + 0.98560028 * daysSinceJ2000
+    val meanLongitude = 280.459 + 0.98564736 * daysSinceJ2000
+    val eclipticLongitude = meanLongitude + 1.915 * sin(meanAnomaly) + 0.020 * sin(2 * meanAnomaly)
+
+    val sunToEarthDistanceAstronomicalUnits = 1.00014 - 0.01671 * cos(meanAnomaly) - 0.00014 * cos(2 * meanAnomaly)
+    val obliquityOfEcliptic = 23.439 - 0.00000036 * daysSinceJ2000
+    val rightAscension = atan2(cos(obliquityOfEcliptic) * sin(eclipticLongitude), cos(eclipticLongitude)) / 15
+
+    val declination = asin(sin(obliquityOfEcliptic) * sin(eclipticLongitude))
+    val equationOfTime = meanLongitude / 15 - rightAscension
 }
