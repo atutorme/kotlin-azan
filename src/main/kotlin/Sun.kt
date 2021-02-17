0187b4adbf205d4c1f538c353edbb9467a4aee72
ATutorMe <atutorme@gmail.com>
2021-02-18 02:00:25 +1000

    fixed bugs in sun calculations

--- src/main/kotlin/Sun.kt
+++ src/main/kotlin/Sun.kt
@@ -10,16 +10,16 @@ class Sun(val date: LocalDate) {
 
     val daysSinceJ2000 = ChronoUnit.DAYS.between(epoch, date)
 
-    val meanAnomaly = 357.529 + 0.98560028 * daysSinceJ2000
-    val meanLongitude = 280.459 + 0.98564736 * daysSinceJ2000
-    val eclipticLongitude = meanLongitude + 1.915 * sin(meanAnomaly.radians()) + 0.020 * sin(2 * meanAnomaly.radians())
+    val meanAnomaly = (357.529 + 0.98560028 * daysSinceJ2000) % 360.0
+    val meanLongitude = (280.459 + 0.98564736 * daysSinceJ2000) % 360.0
+    val eclipticLongitude = (meanLongitude + 1.915 * sin(meanAnomaly.radians()) + 0.020 * sin(2 * meanAnomaly.radians())) % 360.0
 
     val sunToEarthDistanceAstronomicalUnits = 1.00014 - 0.01671 * cos(meanAnomaly.radians()) - 0.00014 * cos(2 * meanAnomaly.radians())
     val obliquityOfEcliptic = 23.439 - 0.00000036 * daysSinceJ2000
-    val rightAscension = atan2(cos(obliquityOfEcliptic.radians()) * sin(eclipticLongitude.radians()), cos(eclipticLongitude.radians())) / 15
+    val rightAscension = (atan2(cos(obliquityOfEcliptic.radians()) * sin(eclipticLongitude.radians()), cos(eclipticLongitude.radians())).degrees() + 360.0) / 15.0
 
-    val declination = asin(sin(obliquityOfEcliptic.radians()) * sin(eclipticLongitude.radians()))
-    val equationOfTime = meanLongitude / 15 - rightAscension
+    val declination = asin(sin(obliquityOfEcliptic.radians()) * sin(eclipticLongitude.radians())).degrees()
+    val equationOfTime = (meanLongitude / 15.0 - rightAscension) * 60.0 // minutes
 }
 
 fun Double.radians() = this * PI / 180.0
--- src/test/kotlin/TestSun.kt
+++ src/test/kotlin/TestSun.kt
@@ -1,7 +1,16 @@
 import java.time.LocalDate
+import java.time.temporal.JulianFields
 
 fun main() {
     val s = Sun(LocalDate.now())
-    println(s.equationOfTime)
-    println(s.daysSinceJ2000)
+    println("daysSinceJ2000 : ${s.daysSinceJ2000}")
+    println("meanAnomaly : ${s.meanAnomaly}")
+    println("meanLongitude : ${s.meanLongitude}")
+    println("eclipticLongitude : ${s.eclipticLongitude}")
+    println("sunToEarthDistanceAstronomicalUnits : ${s.sunToEarthDistanceAstronomicalUnits}")
+    println("obliquityOfEcliptic : ${s.obliquityOfEcliptic}")
+    println("rightAscension : ${s.rightAscension}")
+    println("declination : ${s.declination}")
+    println("equationOfTime : ${s.equationOfTime}")
+    println(JulianFields.JULIAN_DAY.getFrom(LocalDate.now()))
 }
