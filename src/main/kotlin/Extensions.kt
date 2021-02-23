50f6d6d17d8cc83d9b930f842bf9f22d91880683
ATutorMe <atutorme@gmail.com>
2021-02-23 15:35:20 +1000

    started NOAA implementation based on excel file specs

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime
+import java.time.temporal.JulianFields
 import java.util.*
 import kotlin.math.PI
 
@@ -6,3 +8,6 @@ fun Double.degrees() = this * 180.0 / PI
 
 val gregorianCalendar = GregorianCalendar()
 fun Int.isLeapYear() = gregorianCalendar.isLeapYear(this)
+
+fun LocalDateTime.julianDay(timeZone: Double = 0.0) = JulianFields.JULIAN_DAY.getFrom(this) +
+        this.hour / 24.0 + this.minute / (1440.0) - timeZone / 24.0
---
+++ src/main/kotlin/SunNOAA2.kt
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime
+
+/**
+ * Alternate implementation of Solar Calculations
+ * Based on the spreadsheets from the NOAA website:
+ * https://www.esrl.noaa.gov/gmd/grad/solcalc/calcdetails.html
+ */
+class SunNOAA2(val dateTime: LocalDateTime = LocalDateTime.now(), val location: Location = Location()) {
+
+
+}
