3c1faa3448fcb3cf5784bbfc473b0df645a9f855
ATutorMe <atutorme@gmail.com>
2021-02-24 13:42:32 +1000

    extension functions for showing times from fraction of day

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -8,8 +8,10 @@ fun Double.degrees() = this * 180.0 / PI
 val gregorianCalendar = GregorianCalendar()
 fun Int.isLeapYear() = gregorianCalendar.isLeapYear(this)
 
-fun Double.toHMS() : String = "${h}:${m}:${s}"
+fun Double.toHMS() : String = "${h.zeroPad()}:${m.zeroPad()}:${s.zeroPad()}"
 
 val Double.h : Int get() = (this * 24).toInt()
-val Double.m : Int get() = ((this * 60.0) % 60.0).toInt()
-val Double.s : Int get() = ((this * 3600.0) % 60.0).toInt()
+val Double.m : Int get() = ((this * 24 * 60.0) % 60.0).toInt()
+val Double.s : Int get() = ((this * 24 * 3600.0) % 60.0).toInt()
+
+fun Int.zeroPad() : String = this.toString().padStart(2, '0')
