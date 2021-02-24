1ccb2eafb94fdf5b26207e3cb5c7c86060a06061
ATutorMe <atutorme@gmail.com>
2021-02-24 13:28:23 +1000

    debugging formulae

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -8,3 +8,8 @@ fun Double.degrees() = this * 180.0 / PI
 val gregorianCalendar = GregorianCalendar()
 fun Int.isLeapYear() = gregorianCalendar.isLeapYear(this)
 
+fun Double.toHMS() : String = "${h}:${m}:${s}"
+
+val Double.h : Int get() = (this * 24).toInt()
+val Double.m : Int get() = ((this * 60.0) % 60.0).toInt()
+val Double.s : Int get() = ((this * 3600.0) % 60.0).toInt()
