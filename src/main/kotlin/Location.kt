d62920cebb1c4a89241d6bb2c4285b3527798e31
ATutorMe <atutorme@gmail.com>
2021-02-25 14:13:51 +1000

    salat times adjusted for extreme latitudes according to Fiqh Made Easy

--- src/main/kotlin/Location.kt
+++ src/main/kotlin/Location.kt
@@ -1,8 +1,16 @@
+import kotlin.math.absoluteValue
+
 // GPS Coordinates of the Ka'bah - https://www.gps-coordinates.net/
 val MAKKAH_LATTITUDE = 21.422411
 val MAKKAH_LONGITUDE = 39.826218
 val MAKKAH_ALTITUDE = 304.0
 val MAKKAH_TIMEZONE = 3.0
+
+enum class LatitudeCategory(val latitude: Double) {
+    CATEGORY_1(48.0),
+    CATEGORY_2(66.0),
+    CATEGORY_3(90.0)
+}
 
 data class Location (
     val name: String = "Makkah",
@@ -10,4 +18,15 @@ data class Location (
     val longitude: Double = MAKKAH_LONGITUDE,
     val altitude: Double = MAKKAH_ALTITUDE,
     val timeZone: Double = MAKKAH_TIMEZONE,
-)
+) {
+    val latitudeCategory: LatitudeCategory = when {
+        latitude.absoluteValue <= LatitudeCategory.CATEGORY_1.latitude -> LatitudeCategory.CATEGORY_1
+        latitude.absoluteValue <= LatitudeCategory.CATEGORY_2.latitude -> LatitudeCategory.CATEGORY_2
+        else -> LatitudeCategory.CATEGORY_3
+    }
+
+    fun closestLocationForExtremeLatitudes() : Location = when(latitudeCategory) {
+        LatitudeCategory.CATEGORY_1 -> Location(name, latitude, longitude, altitude, timeZone)
+        else -> Location(name, LatitudeCategory.CATEGORY_1.latitude, longitude, altitude, timeZone)
+    }
+}
--- src/main/kotlin/SalatTimes.kt
+++ src/main/kotlin/SalatTimes.kt
@@ -1,29 +1,61 @@
 import java.time.LocalDateTime
 import kotlin.math.*
 
+enum class SalatNames(val niceName: String) {
+    FAJR("Fajr"),
+    SUNRISE("Sunrise"),
+    DHUHR("Dhuhr"),
+    ASR("Asr"),
+    MAGHRIB("Maghrib"),
+    ISHA("Isha")
+}
+
 class SalatTimes(val dateTime: LocalDateTime = LocalDateTime.now(),
                  val location: Location = Location(),
                  val calculationMethod: CalculationMethod = CalculationMethod.MUSLIM_WORLD_LEAGUE,
                  val juristicMethod: JuristicMethod = JuristicMethod.MAJORITY,
-                 val adjustForAltitude: Boolean = true
+                 val adjustForAltitude: Boolean = true,
+                 val adjustForExtremeLatitudes: Boolean = true
 ) {
-    val sunNOAA2 = Sun(dateTime, location)
+    val sun = Sun(dateTime, location)
+    val sunExtreme = Sun(dateTime, location.closestLocationForExtremeLatitudes())
 
-    val sunrise = if (adjustForAltitude) sunNOAA2.sunriseTimeAltitudeCorrected else sunNOAA2.sunriseTime
-    val sunset = if (adjustForAltitude) sunNOAA2.sunsetTimeAltitudeCorrected else sunNOAA2.sunsetTime
+    val sunrise = if (adjustForAltitude) sun.sunriseTimeAltitudeCorrected else sun.sunriseTime
+    val sunset = if (adjustForAltitude) sun.sunsetTimeAltitudeCorrected else sun.sunsetTime
 
-    val dhuhr = sunNOAA2.solarNoon
+    val sunriseExtreme = if (adjustForAltitude) sunExtreme.sunriseTimeAltitudeCorrected else sunExtreme.sunriseTime
+    val sunsetExtreme = if (adjustForAltitude) sunExtreme.sunsetTimeAltitudeCorrected else sunExtreme.sunsetTime
+
+    val dhuhr = sun.solarNoon
     val maghrib = sunset
 
-    val fajr = dhuhr - sunNOAA2.t(calculationMethod.fajrAngle, adjustForAltitude) / 360.0
-    val isha = if (calculationMethod.overrideIshaDelayMins == null) dhuhr + sunNOAA2.t(calculationMethod.ishaAngle, adjustForAltitude) / 360.0
+    val fajr = dhuhr - sun.t(calculationMethod.fajrAngle, adjustForAltitude) / 360.0
+    val isha = if (calculationMethod.overrideIshaDelayMins == null) dhuhr + sun.t(calculationMethod.ishaAngle, adjustForAltitude) / 360.0
     else maghrib + calculationMethod.overrideIshaDelayMins
 
-    val asr = dhuhr + a(juristicMethod.shadowLength)
+    val asr = dhuhr + a(juristicMethod.shadowLength, sun)
 
-    fun a(shadowLength: Double) : Double = acos(sin(acot(shadowLength + tan((location.latitude - sunNOAA2.sunDeclin).radians()))) /
-                (cos(location.latitude.radians()) * cos(sunNOAA2.sunDeclin.radians())) -
-                tan(location.latitude.radians()) * tan(sunNOAA2.sunDeclin.radians())).degrees() / 360.0
+    val dhuhrExtreme = sunExtreme.solarNoon
+    val maghribExtreme = sunsetExtreme
+
+    val fajrExtreme = dhuhrExtreme - sunExtreme.t(calculationMethod.fajrAngle, adjustForAltitude) / 360.0
+    val ishaExtreme = if (calculationMethod.overrideIshaDelayMins == null) dhuhrExtreme + sunExtreme.t(calculationMethod.ishaAngle, adjustForAltitude) / 360.0
+    else maghribExtreme + calculationMethod.overrideIshaDelayMins
+
+    val asrExtreme = dhuhrExtreme + a(juristicMethod.shadowLength, sunExtreme)
+
+    val salatTimestamps: Map<String, String> get() = mapOf(
+        SalatNames.FAJR.niceName to if (adjustForExtremeLatitudes) fajrExtreme.toHMS() else fajr.toHMS(),
+        SalatNames.SUNRISE.niceName to if (adjustForExtremeLatitudes && location.latitudeCategory == LatitudeCategory.CATEGORY_3) sunriseExtreme.toHMS() else sunrise.toHMS(),
+        SalatNames.DHUHR.niceName to if (adjustForExtremeLatitudes && location.latitudeCategory == LatitudeCategory.CATEGORY_3) dhuhrExtreme.toHMS() else dhuhr.toHMS(),
+        SalatNames.ASR.niceName to if (adjustForExtremeLatitudes && location.latitudeCategory == LatitudeCategory.CATEGORY_3) asrExtreme.toHMS() else asr.toHMS(),
+        SalatNames.MAGHRIB.niceName to if (adjustForExtremeLatitudes && location.latitudeCategory == LatitudeCategory.CATEGORY_3) maghribExtreme.toHMS() else maghrib.toHMS(),
+        SalatNames.ISHA.niceName to if (adjustForExtremeLatitudes) ishaExtreme.toHMS() else isha.toHMS(),
+    )
+
+    fun a(shadowLength: Double, sun: Sun) : Double = acos(sin(acot(shadowLength + tan((location.latitude - sun.sunDeclin).radians()))) /
+                (cos(location.latitude.radians()) * cos(sun.sunDeclin.radians())) -
+                tan(location.latitude.radians()) * tan(sun.sunDeclin.radians())).degrees() / 360.0
 }
 
 fun acot(x: Double) : Double = atan(1.0 / x)
