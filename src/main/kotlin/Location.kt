8c79d036dfd353f380e47c042b9588168045cd49
ATutorMe <atutorme@gmail.com>
2021-03-01 09:33:08 +1000

    makkah constants into location companion object

--- src/main/kotlin/Location.kt
+++ src/main/kotlin/Location.kt
@@ -1,11 +1,5 @@
 import kotlin.math.absoluteValue
 
-// GPS Coordinates of the Ka'bah - https://www.gps-coordinates.net/
-val MAKKAH_LATTITUDE = 21.422411
-val MAKKAH_LONGITUDE = 39.826218
-val MAKKAH_ALTITUDE = 304.0
-val MAKKAH_TIMEZONE = 3.0
-
 enum class LatitudeCategory(val latitude: Double) {
     CATEGORY_1(48.0),
     CATEGORY_2(66.0),
@@ -19,6 +13,14 @@ data class Location (
     val altitude: Double = MAKKAH_ALTITUDE,
     val timeZone: Double = MAKKAH_TIMEZONE,
 ) {
+    companion object {
+        // GPS Coordinates of the Ka'bah - https://www.gps-coordinates.net/
+        const val MAKKAH_LATTITUDE = 21.422411
+        const val MAKKAH_LONGITUDE = 39.826218
+        const val MAKKAH_ALTITUDE = 304.0
+        const val MAKKAH_TIMEZONE = 3.0
+    }
+
     val latitudeCategory: LatitudeCategory = when {
         latitude.absoluteValue <= LatitudeCategory.CATEGORY_1.latitude -> LatitudeCategory.CATEGORY_1
         latitude.absoluteValue <= LatitudeCategory.CATEGORY_2.latitude -> LatitudeCategory.CATEGORY_2
