0640f32cea999628ae824da99407a17ad4af4982
ATutorMe <atutorme@gmail.com>
2021-05-19 23:19:54 +1000

    cli azan player

---
+++ src/main/kotlin/AzanPlayerCli.kt
@@ -0,0 +1,33 @@
+import java.util.*
+import kotlin.concurrent.timerTask
+
+fun main() {
+    val location = Location(
+        "Brisbane",
+        -27.468968,
+        153.023499,
+        17.0,
+        10.0
+    )
+
+    val salatTimes = SalatTimes(location = location)
+
+    val timer = Timer("Azan Player Cli", false)
+
+    scheduleNextAzan(salatTimes, timer)
+}
+
+fun scheduleNextAzan(salatTimes: SalatTimes, timer: Timer) {
+    println("Scheduling salat: ${salatTimes.nextSalatTime()}")
+    println("in: ${salatTimes.secondsToNextSalat()} seconds")
+
+    timer.schedule(
+        timerTask {
+            println("It's azan time!")
+            println("Salat: ${salatTimes.currentSalatTime()}")
+
+            scheduleNextAzan(SalatTimes(location = salatTimes.location), timer)
+
+        }, salatTimes.secondsToNextSalat() * 1000
+    )
+}
