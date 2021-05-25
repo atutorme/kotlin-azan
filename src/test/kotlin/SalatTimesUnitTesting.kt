79922c68af64cfbd5011b3c680ce91c60f4e4d72
ATutorMe <atutorme@gmail.com>
2021-05-25 20:59:06 +1000

    fixed up unit tests to test locations in different quadrants of latitude and longitude

--- src/test/kotlin/SalatTimesUnitTesting.kt
+++ src/test/kotlin/SalatTimesUnitTesting.kt
@@ -1,22 +1,35 @@
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+
+
 class SalatTimesUnitTesting {
+
     @Test
-    fun testNextSalat() {
-        val allSalatDateTimes = SalatTimes().salatDateTimes
+    fun testSalatTimes() {
 
-        allSalatDateTimes.forEach(::println)
+        val salatNiceNames: List<String> = SalatNames.values().map { it.niceName }
 
-        var i = 0
-        val salatNiceNames = SalatNames.values().map { it.niceName }
+        val locations: List<Location> = listOf(
+            Location(), // Makkah
+            Location("Islamabad", 33.6938118, 73.0651511, 532.0, 5.0),
+            Location("Sydney", -33.8548157, 151.2164539, 7.0, 10.0),
+            Location("New York", 40.741895, -73.989308, 13.0, -4.0),
+            Location("Buenos Aires", -34.6075682, -58.4370894, 25.0, -3.0)
+        )
+
+        val allSalatDateTimes = locations.map { SalatTimes(location = it) }
 
         allSalatDateTimes.forEach {
-            val localDateTimeProvider = LocalDateTimeProvider { it.value.minusMinutes(1) }
-            val nextSalat = SalatTimes(localDateTimeProvider).nextSalatTime(localDateTimeProvider)
-            Assertions.assertEquals(salatNiceNames[i], nextSalat.first.removeYesterdays())
-            i++
-            i %= salatNiceNames.size
+            val sNames = it.salatDateTimes
+                .toList()
+                .sortedBy { s -> s.second }
+                .map { s -> s.first }
+
+            println(it.location.name)
+            println(sNames)
+
+            assertIterableEquals(salatNiceNames, sNames)
         }
 
     }
